c75ca36f6700ff0afcacf0b1f6a68155740fd967
completely-burned <your.favorite.crazy@gmail.com>
2020-02-08 11:46:11 +0300

    AddonBuilder.ps1

---
+++ tools/AddonBuilder.ps1
@@ -0,0 +1,21 @@
+
+New-Item -Force -ItemType Directory -Path "..\.build.tmp"
+New-Item -Force -ItemType Directory -Path "..\.build.out"
+
+$env_pbo=Resolve-Path ..\.build.out\
+$env_includes=Resolve-Path .\pbo_include.txt
+
+New-Item -Force -ItemType Directory -Path ..\.build.tmp\glowing-octo-shame-east-dev.chernarus\
+
+$env_pre=Resolve-Path ..\.build.tmp\glowing-octo-shame-east-dev.chernarus\
+
+Remove-Item -Path ..\.build.tmp\glowing-octo-shame-east-dev.chernarus\* -Recurse
+
+Copy-Item -Recurse -Path "..\glowing-octo-shame-east.Chernarus\*" -Destination $env_pre
+Copy-Item -Recurse -Path "..\glowing-octo-shame-arma2\*" -Destination $env_pre
+
+$AddonBuilderRegKey = "HKLM:\SOFTWARE\WOW6432Node\Bohemia Interactive\AddonBuilder"
+
+$AddonBuilderExe = join-path -path (Get-ItemProperty $AddonBuilderRegKey)."path" -childpath (Get-ItemProperty $AddonBuilderRegKey)."exe"
+
+& $AddonBuilderExe $env_pre $env_pbo -clear -include="$env_includes"
