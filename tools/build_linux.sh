d74afec31872cf47ecf5fc97c9c67445b74963b1
completely-burned <4268642+completely-burned@users.noreply.github.com>
2020-08-14 12:38:55 +0300

    Update build_linux.sh

--- tools/build_linux.sh
+++ tools/build_linux.sh
@@ -16,16 +16,20 @@ fi
 
 for DIR in $(find ./ -maxdepth 1 -type d); do
 	if [ -f "${DIR}/mission.sqm" ]; then
+
 		VERSION=$(grep briefingName ${DIR}/mission.sqm | sed -e 's/.*".* .* .* v\(.*\)".*/\1/' -e 's/\./\-/gi')
 		SIDE=$(grep briefingName ${DIR}/mission.sqm | sed -e 's/.*".* .* \(.*\) v.*".*/\1/')
 		NAME=$(grep briefingName ${DIR}/mission.sqm | sed -e 's/.*"\(.*\) .* .* v.*".*/\1/')
 		MAP=$(echo ${DIR} | sed -e 's/.*\.\(.*\)/\1/')
+
 		TMPDIRNAME="${NAME,,}-${VERSION,,}-${SIDE,,}.${MAP,,}"
+
 		if [ -d ".build.tmp/${TMPDIRNAME}/" ]; then
 			rm -rf ".build.tmp/${TMPDIRNAME}/*"
 		else
 			mkdir ".build.tmp/${TMPDIRNAME}/"
 		fi
+
 		find glowing-octo-shame-arma2/ -mindepth 1 -maxdepth 1 -exec ln -snf ../../{} ".build.tmp/${TMPDIRNAME}/" \;
 		find ${DIR} -mindepth 1 -maxdepth 1 -exec ln -snf ../../{} ".build.tmp/${TMPDIRNAME}/" \;
 
