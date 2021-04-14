d76eb894c6d074bbb64be99c1d66c9f457529092
completely-burned <4268642+completely-burned@users.noreply.github.com>
2021-04-14 18:56:30 +0300

    tools/build_pbo_linux.sh use gnu parallel and change cp to rsync

--- tools/build_pbo_linux.sh
+++ tools/build_pbo_linux.sh
@@ -34,19 +34,30 @@ for DIR in $(find ./ -maxdepth 1 -type d); do
 		#find glowing-octo-shame-arma2/ -mindepth 1 -maxdepth 1 -exec ln -snf ../../{} ".build.tmp/${TMPDIRNAME}/" \;
 		#find ${DIR} -mindepth 1 -maxdepth 1 -exec ln -snf ../../{} ".build.tmp/${TMPDIRNAME}/" \;
 
-		# делаем копию чтобы изменить файлы не затронув git директорию перед сборкой .pbo
-		cp -r glowing-octo-shame-arma2/* .build.tmp/${TMPDIRNAME}/
-		cp -r ${DIR}/* .build.tmp/${TMPDIRNAME}/
+		# делаем rsync чтобы изменять копии файлов не затронув git директорию перед сборкой .pbo
+		rsync --recursive glowing-octo-shame-arma2/* .build.tmp/${TMPDIRNAME}/
+		rsync --recursive ${DIR}/* .build.tmp/${TMPDIRNAME}/
 
 		# строки начинающиеся с diag_log нужны для отладки
 		# они возможно снижают производительность
 		# поэтому удаляем их
 		find .build.tmp/${TMPDIRNAME}/ -type f -exec sed -i "/^.*diag_log.*/d" {} \;
 
-		# сборка .pbo
-		makepbo -M .build.tmp/${TMPDIRNAME}/ .build.out/${NAME,,}-${VERSION,,}-${SIDE,,}-makepbo.${MAP,,}.pbo &>/dev/null
-		armake build --packonly --force .build.tmp/${TMPDIRNAME}/ .build.out/${NAME,,}-${VERSION,,}-${SIDE,,}-armake.${MAP,,}.pbo &>/dev/null
-		armake2 pack -v .build.tmp/${TMPDIRNAME}/ .build.out/${NAME,,}-${VERSION,,}-${SIDE,,}-armake2.${MAP,,}.pbo &>/dev/null
+		# если установлен gnu parallel можно запустить несколько комманд паралельно, предварительно их подготовив
+		if [ -x "$(command -v parallel)" ]; then
+			var_parallel+=("makepbo -M .build.tmp/${TMPDIRNAME}/ .build.out/${NAME,,}-${VERSION,,}-${SIDE,,}-makepbo.${MAP,,}.pbo")
+			var_parallel+=("armake build --packonly --force .build.tmp/${TMPDIRNAME}/ .build.out/${NAME,,}-${VERSION,,}-${SIDE,,}-armake.${MAP,,}.pbo")
+			var_parallel+=("armake2 pack -v .build.tmp/${TMPDIRNAME}/ .build.out/${NAME,,}-${VERSION,,}-${SIDE,,}-armake2.${MAP,,}.pbo")
+		else
+			makepbo -M .build.tmp/${TMPDIRNAME}/ .build.out/${NAME,,}-${VERSION,,}-${SIDE,,}-makepbo.${MAP,,}.pbo
+			armake build --packonly --force .build.tmp/${TMPDIRNAME}/ .build.out/${NAME,,}-${VERSION,,}-${SIDE,,}-armake.${MAP,,}.pbo
+			armake2 pack -v .build.tmp/${TMPDIRNAME}/ .build.out/${NAME,,}-${VERSION,,}-${SIDE,,}-armake2.${MAP,,}.pbo
+		fi
 
 	fi
 done
+
+#gnu parallel
+if [ ! -z "$var_parallel" ]; then
+  parallel ::: "${var_parallel[@]}"
+fi
