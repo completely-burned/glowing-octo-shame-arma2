0f153f91712088d9c2de42cbbd98fb50f5990065
completely-burned <4268642+completely-burned@users.noreply.github.com>
2022-02-12 04:05:39 +0300

    note

---
+++ tools/note.txt.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo "в этом файле заметки, можете открыть его в текстовом редакторе"
+exit 1
+
+####################################################################
+## заметки ## заметки ## заметки ## заметки ## заметки ## заметки ##
+####################################################################
+
+cd /media/rsync_or_usb_dir
+
+# релиз
+OUT="~/a2-server/mpmissions/" ./tools/build_pbo_linux.sh
+
+# дебаг
+rm ~/a2oa-server-private/mpmissions/* ; OUT="~/a2oa-server-private/mpmissions/" ./tools/build_pbo_linux.sh -d
+
+# #define __ARMA3__ необходимо установить самостоятельно до версии 2.02
+find ./ -type f -exec sed -i '1s/^/#define __ARMA3__\n/' {} \;
