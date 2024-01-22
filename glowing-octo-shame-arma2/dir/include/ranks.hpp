324fe817173aad42088fed94b8a903bbaada9f2b
user-hq <111693015+user-hq@users.noreply.github.com>
2024-01-22 16:08:03 +0300

    Исправление опечаток cfg в модификациях

--- glowing-octo-shame-arma2/dir/include/ranks.hpp
+++ glowing-octo-shame-arma2/dir/include/ranks.hpp
@@ -1,6 +1,7 @@
 #define "PRIVATE" 0
 #define "CORPORAL" 1
 #define "SERGEANT" 2
+#define "SERGANT" 2
 #define "LIEUTENANT" 3
 #define "CAPTAIN" 4
 #define "MAJOR" 5
@@ -9,6 +10,7 @@
 #define "private" 0
 #define "corporal" 1
 #define "sergeant" 2
+#define "sergant" 2
 #define "lieutenant" 3
 #define "captain" 4
 #define "major" 5
@@ -17,6 +19,7 @@
 #define "Private" 0
 #define "Corporal" 1
 #define "Sergeant" 2
+#define "Sergant" 2
 #define "Lieutenant" 3
 #define "Captain" 4
 #define "Major" 5
