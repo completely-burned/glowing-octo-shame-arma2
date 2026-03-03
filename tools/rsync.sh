55d64884a59ac849d6699013ac58a3d8d220f8fe
in7j0 <205167430+in7j0@users.noreply.github.com>
2026-03-03 03:48:30 +0300

    Update rsync.sh

--- tools/rsync.sh
+++ tools/rsync.sh
@@ -7,30 +7,32 @@ DIR_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 
 OUT="${OUT:-${DIR_ROOT}/.build.out/rsync}"
 
-NAME="glowing-octo-shame"
+NAME="gosa"
 
-game="arma3"
-for DIR in $(find ${DIR_ROOT}/maps/${game} -maxdepth 1 -type d); do
-	if [ -f "${DIR}/mission.sqm" ]; then
-		MAP=$(echo ${DIR} | sed -e 's/.*\.\(.*\)/\1/')
-		TMP=$(grep briefingName ${DIR}/mission.sqm)
-		#pbo_VERSION=$(echo ${TMP} | sed -e 's/.*".*gosa.* \(v.*[[:digit:]]\).*/\1/' -e 's/\./\-/gi')
-		pbo_VERSION="debug-rsync"
-		SIDE=$(echo ${TMP} | sed -e 's/.*".*gosa.* .* \(.*\) v.*".*/\1/')
-		DLC2=$(echo ${TMP} | sed -e 's/.*"\(.*\)CO.*".*/\1/')
-		DLC=$(echo "${DLC2}" | sed -e 's/\ /_/gi')
+games="arma3 a2oa"
+for game in ${games}; do
+	for DIR in $(find ${DIR_ROOT}/maps/${game} -maxdepth 1 -type d); do
+		if [ -f "${DIR}/mission.sqm" ]; then
+			MAP=$(echo ${DIR} | sed -e 's/.*\.\(.*\)/\1/')
+			TMP=$(grep briefingName ${DIR}/mission.sqm)
+			#pbo_VERSION=$(echo ${TMP} | sed -e 's/.*".*gosa.* \(v.*[[:digit:]]\).*/\1/' -e 's/\./\-/gi')
+			pbo_VERSION="debug-rsync"
+			SIDE=$(echo ${TMP} | sed -e 's/.*".*gosa.* .* \(.*\) v.*".*/\1/')
+			DLC2=$(echo ${TMP} | sed -e 's/.*"\(.*\)CO.*".*/\1/')
+			DLC=$(echo "${DLC2}" | sed -e 's/\ /_/gi')
 
-		TMPDIRNAME="${DLC,,}co_00_${NAME,,}-${game,,}-${SIDE,,}-${pbo_VERSION,,}.${MAP,,}"
+			TMPDIRNAME="${DLC,,}co_00_${NAME,,}-${game,,}-${SIDE,,}-${pbo_VERSION,,}.${MAP,,}"
 
-		MISSION="${OUT}/${TMPDIRNAME}"
+			MISSION="${OUT}/${TMPDIRNAME}"
 
-		mkdir -p "${MISSION}"
-		if [[ $NAME != *"compat"* ]]; then
-			rsync --recursive --delete --checksum "${DIR_ROOT}"/glowing-octo-shame-arma2/ "${MISSION}"/
-			rsync --recursive --delete --checksum "${DIR}"/* "${MISSION}"/
+			mkdir -p "${MISSION}"
+			if [[ $NAME != *"compat"* ]]; then
+				rsync --recursive --delete --checksum "${DIR_ROOT}"/glowing-octo-shame-arma2/ "${MISSION}"/
+				rsync --recursive --delete --checksum "${DIR}"/* "${MISSION}"/
+			fi
+
+			# find $MISSION -type f -exec sed -i '1s/^/#define __ARMA3__\n/' {} \;
+
 		fi
-
-		# find $MISSION -type f -exec sed -i '1s/^/#define __ARMA3__\n/' {} \;
-
-	fi
+	done
 done
